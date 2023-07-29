aeebd8493eb655e4c9fbdba161e04d6629c3e5cf
Ricardo <dude5056@gmail.com>
2023-07-28 19:38:50 -0700

    Newer Website

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+let topButton = document.getElementById("tButton");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        
+        // topButton.style.display = "block";
+        topButton.style.opacity = 0.9;
+
+    }
+        else {
+            //topButton.style.display = "none";
+        topButton.style.opacity = 0.1;
+    }
+}
+function topFunc() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
